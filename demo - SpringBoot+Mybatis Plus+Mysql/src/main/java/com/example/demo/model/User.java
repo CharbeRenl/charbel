5834d8aecad0f4b379fb958a767a937f74e4e223
rc <rencong1991@yeah.net>
2019-11-25 10:06:44 +0800

    提交项目

---
+++ demo - SpringBoot+Mybatis Plus+Mysql/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 用户表
+ * </p>
+ *
+ * @author rc
+ * @since 2019-11-22
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户ID
+     */
+    private Long id;
+    /**
+     * 用户名
+     */
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+        ", id=" + id +
+        ", name=" + name +
+        "}";
+    }
+}
---
+++ demo - SpringBoot+Mybatis Plus+Mysql/src/main/java/com/example/demo/web/UserController.java
@@ -0,0 +1,21 @@
+package com.example.demo.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author rc
+ * @since 2019-11-22
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
+
